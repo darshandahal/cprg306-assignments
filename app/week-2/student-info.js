0caa1dab3a5144f8711b43281c8eb31065c85833
darshan <darshandahal304@gmail.com>
2024-09-18 13:57:41 -0600

    week2
    added a week2 folder and created some changes and adding my github repository link

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return (
+        <main>
+        <h1>Darshan Dahal</h1>
+        <Link href="https://github.com/darshandahal?tab=repositories">GitHub repository</Link>
+        </main>
+    )
+}
