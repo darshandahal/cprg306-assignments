a98d9b7eeb362cbe1f9af39ca49203f510a35a00
darshan <darshandahal304@gmail.com>
2024-11-08 09:20:50 -0700

    week-8 partially complete

---
+++ app/week-8/item-list.js
@@ -0,0 +1,38 @@
+"use client";
+import Item from './item.js';
+import {useState} from 'react';
+
+export function ItemList({ items }) {
+
+    const [sortBy, setSortBy] = useState('name');
+
+    // items.sort ((a, b) => a.name.localeCompare(b.name)); // sort by name
+    // items.sort ((a, b) => a.category.localeCompare(b.category)); // sort by category
+
+    const sortedItems = [...items].sort((a, b) => {
+        if (sortBy === 'name') {
+            return a.name.localeCompare(b.name);
+        } else if (sortBy === 'category') {
+            return a.category.localeCompare(b.category);
+        }
+        return 0;
+    });
+
+
+    return (
+        <main className="m-4">
+
+            <div className="flex space-x-2 mb-4">
+            <button className={`px-4 py-2 rounded-xl h-full  ${sortBy === 'name' ? "bg-blue-700" : "bg-gray-700 && hover:bg-green-900"} text-white`}onClick={() => setSortBy('name')}>Sort by Name </button>
+            <button className={`px-4 py-2 rounded-xl h-full  ${sortBy === 'category' ? "bg-blue-700" : "bg-gray-700 && hover:bg-green-900"} text-white`} onClick={() => setSortBy('category')}>Sort by Category</button>
+            </div>
+            {items.map((item, id) => (
+                <Item
+                    key={id}
+                    name={item.name}
+                    category={item.category}
+                />
+            ))}
+        </main>
+    );
+}
